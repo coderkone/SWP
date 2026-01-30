da73a9204d9a17fe5459266cf64a25478d432614
coderkone <kiritovsalice@gmail.com>
2026-01-30 11:28:00 +0700

    userDTO

---
+++ src/java/dto/UserDTO.java
@@ -0,0 +1,33 @@
+package dto;
+
+public class UserDTO {
+    private long userId;
+    private String username;
+    private String email;
+    private String role;
+
+    public UserDTO() {}
+
+    public UserDTO(long userId, String username, String email, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+
+    public long getUserId() { return userId; }
+    public void setUserId(long userId) { this.userId = userId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+}
+
+
+
+
