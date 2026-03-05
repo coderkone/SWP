b523cdbdb44e49330e2d326431a0bf1af06ea3dc
coderkone <kiritovsalice@gmail.com>
2026-03-05 21:36:35 +0700

    insert data

--- Database/DB-DevQuery.sql
+++ Database/DB-DevQuery.sql
@@ -140,6 +140,8 @@ CREATE TABLE [dbo].[Bookmarks](
 )
 GO
 
+
+
 -- BADGES
 CREATE TABLE [dbo].[Badges](
 	[badge_id] [bigint] IDENTITY(1,1) PRIMARY KEY,
@@ -255,3 +257,16 @@ CREATE TABLE [dbo].[UserFollow](
 )
 GO
 
+-- Add accepted_answer_id to Questions (Option A - recommended)
+-- When accepted answer is deleted, ON DELETE SET NULL clears it automatically.
+
+USE [devquery]
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID('dbo.Questions') AND name = 'accepted_answer_id')
+BEGIN
+    ALTER TABLE [dbo].[Questions] ADD [accepted_answer_id] [bigint] NULL;
+    ALTER TABLE [dbo].[Questions] ADD CONSTRAINT [FK_Questions_AcceptedAnswer] 
+        FOREIGN KEY ([accepted_answer_id]) REFERENCES [dbo].[Answers]([answer_id]) ON DELETE SET NULL;
+END
+GO
